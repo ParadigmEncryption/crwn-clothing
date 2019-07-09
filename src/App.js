6c7fa70ed0cb623962a605abb9f516f616b98d93
Paradigm <ParadigmEncryption@gmail.com>
2019-07-08 20:10:30 -0500

    added routing functionality

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
 
 import './App.css';
+
+const HatsPage = (props) => (
+  <div>
+    {console.log(props)}
+    <h1>HATS</h1>
+  </div>
+)
 
 class App extends React.Component {
   
   render() {
     return (
       <div className="App">
-        <HomePage />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop/hats' component={HatsPage} />
+        </Switch>
       </div>
     );
   }
