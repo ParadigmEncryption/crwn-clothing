7341096ee9e4452cfba2a07d177c8d60ee785ab3
Paradigm <ParadigmEncryption@gmail.com>
2019-08-06 17:20:08 -0500

    implemented userReducer and userAction in Header and App components

--- src/App.js
+++ src/App.js
@@ -12,39 +12,28 @@ import { setCurrentUser } from './redux/user/user.actions';
 import './App.css';
 
 class App extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      currentUser: null
-    }
-  }
-
   unsubscribeFromAuth = null;
 
   componentDidMount() {
+  const { setCurrentUser } = this.props;  // import and access redux action via props
+
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => { // built-in function
       if (userAuth) { // check if Snapshot has changed (probably not, since we're not changing it here)
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShot => { // built-in function, similar to calling onAuthStateChanged
-          this.setState(
-            {
-              currentUser: {
-                id: snapShot.id,
-                ...snapShot.data()
-              } // get id from Snapshot, and displayName, email and createdAt from our database
-            }, 
-            () => {
-              console.log(this.state); // 2nd parameter of setState makes sure the state has updated before running console.log
-            }
-          ); // get data stored in snapShot, whether user exists in database already, or if new user is created
+          setCurrentUser({ // setCurrentUser replaces this.setState
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            } // get id from Snapshot, and displayName, email and createdAt from our database
+          }); // get data stored in snapShot, whether user exists in database already, or if new user is created
         });
       } else {
-        this.setState({ currentUser: userAuth });
-      }
-    });
-  }
+        setCurrentUser(userAuth); 
+      }   // replaces: this.setState({ currentUser: userAuth });
+  })
+}
 
   componentWillUnmount() {
     this.unsubscribeFromAuth();
@@ -66,7 +55,7 @@ class App extends React.Component {
 }
 
 const mapDispatchToProps = dispatch => ({
-  setCurrentUser: dispatch
+  setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
 export default connect(null, mapDispatchToProps)(App);
